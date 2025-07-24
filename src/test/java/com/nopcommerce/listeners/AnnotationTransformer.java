eac917b824ff8f7ba3d2c74fa5e55d1e973b2c54
flopezs97 <flopezs301297@gmail.com>
2025-07-24 01:21:36 -0500

    Creating BasePage.java, BaseTest.java and logs.

--- src/test/java/com/nopcommerce/listeners/AnnotationTransformer.java
+++ src/test/java/com/nopcommerce/listeners/AnnotationTransformer.java
@@ -8,6 +8,6 @@ import java.lang.reflect.Method;
 
 public class AnnotationTransformer implements IAnnotationTransformer {
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
-        annotation.setRetryAnalyzer();
+        annotation.setRetryAnalyzer(Retry.class);
     }
 }

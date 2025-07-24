700a2d8593891af462d514c238361d6d456dc38e
flopezs97 <flopezs301297@gmail.com>
2025-07-24 01:27:15 -0500

    Creating util that take screens when it takes the order.

--- src/test/java/com/nopcommerce/utils/Utils.java
+++ src/test/java/com/nopcommerce/utils/Utils.java
@@ -1,5 +1,16 @@
 package com.nopcommerce.utils;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Utils {
-    
+    public static void takeScreenOrder(WebDriver driver, String idOrder) throws IOException {
+        File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(source, new File("./screenshots/" + idOrder + ".jpg"));
+    }
 }
